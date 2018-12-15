de43594d241206c60ab95f3f51643c0bd974a6bf
Samuel Giddins <segiddins@squareup.com>
2018-12-14 16:58:33 -0800

    Remove dead code

--- 2018/day_14.rb
+++ 2018/day_14.rb
@@ -1,10 +1,5 @@
 #!/usr/bin/env ruby
 
-
-
-elf1 = []
-elf2 = []
-
 scoreboard = [3, 7]
 
 elf1_idx = 0
@@ -22,11 +17,11 @@ def print_scoreboard(scoreboard, e1, e2)
 end
 
 20403320.times do
-#  print_scoreboard(scoreboard, elf1_idx, elf2_idx)
+  # print_scoreboard(scoreboard, elf1_idx, elf2_idx)
   scoreboard.concat (scoreboard[elf1_idx] + scoreboard[elf2_idx]).digits.reverse
   elf1_idx = (elf1_idx + 1 + scoreboard[elf1_idx]) % scoreboard.size
   elf2_idx = (elf2_idx + 1 + scoreboard[elf2_idx]) % scoreboard.size
 end
 
-puts scoreboard[909441, 10].join
+puts(scoreboard[909441, 10].join)
 puts(scoreboard.join =~ /909441/)
