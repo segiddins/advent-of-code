44774b49121fab47e760657f7ba710bf28fd358f
Samuel Giddins <segiddins@squareup.com>
2020-12-07 21:13:47 -0800

    RuboCop

--- 2020/day_8.rb
+++ 2020/day_8.rb
@@ -7,25 +7,26 @@ $input = File.read(__FILE__.sub(/\.rb\z/, '.txt'))
 $input_lines = $input.split("\n")
 
 VM = Struct.new(:accumulator, :rip, :inst, :ran) do
-    def run
-        loop do
-            i, a, = *inst[rip]
-            return [:term, accumulator] unless i
-            return [:looped, accumulator] unless ran.add?(rip)
-            case i
-            when 'acc'
-                self.accumulator += a
-            when 'jmp'
-                self.rip += a
-                next
-            when 'nop'
-                nil
-            else
-                raise "unknown #{inst[rip]} #{i} #{a}"
-            end
-            self.rip += 1
-        end
+  def run
+    loop do
+      i, a, = *inst[rip]
+      return [:term, accumulator] unless i
+      return [:looped, accumulator] unless ran.add?(rip)
+
+      case i
+      when 'acc'
+        self.accumulator += a
+      when 'jmp'
+        self.rip += a
+        next
+      when 'nop'
+        nil
+      else
+        raise "unknown #{inst[rip]} #{i} #{a}"
+      end
+      self.rip += 1
     end
+  end
 end
 
 $vm = VM.new(0, 0, $input_lines.map { |l| i, a = l.split(' ', 2); [i, a&.to_i] }, Set.new)
@@ -35,20 +36,20 @@ def part1
 end
 
 def part2
-    inst = $input_lines.map { |l| i, a = l.split(' ', 2); [i, a&.to_i] }
-    inst.count.times do |i|
-        ins = inst.dup
-        is = inst[i].dup
-        if is[0] == 'jmp'
-            is[0] = 'nop'
-        elsif is[0] == 'nop'
-            is[0] = 'jmp'
-        end
-        ins[i] = is
-        vm = VM.new(0, 0, ins, Set.new)
-        a, b = vm.run
-        return b if a == :term
+  inst = $input_lines.map { |l| i, a = l.split(' ', 2); [i, a&.to_i] }
+  inst.count.times do |i|
+    ins = inst.dup
+    is = inst[i].dup
+    if is[0] == 'jmp'
+      is[0] = 'nop'
+    elsif is[0] == 'nop'
+      is[0] = 'jmp'
     end
+    ins[i] = is
+    vm = VM.new(0, 0, ins, Set.new)
+    a, b = vm.run
+    return b if a == :term
+  end
 end
 
 pp part1
