d1e76f71af7f4f9bbbd549e3ba1b6b3bdfc227e8
Samuel Giddins <segiddins@squareup.com>
2020-12-11 21:22:35 -0800

    RuboCop

--- 2020/day_12.rb
+++ 2020/day_12.rb
@@ -4,28 +4,28 @@ require_relative '../aoc'
 
 $input = File.read(__FILE__.sub(/\.rb\z/, '.txt'))
 # $input = DATA.read
-$input_lines = $input.split("\n").map { |l| l=~/(.)(\d+)/; [$1, $2.to_i] }
+$input_lines = $input.split("\n").map { |l| l =~ /(.)(\d+)/; [Regexp.last_match(1), Regexp.last_match(2).to_i] }
 
 def part1
-  pos  = 0i
-  dir = 1+0i
+  pos = 0i
+  dir = 1 + 0i
 
   $input_lines.each do |a, c|
     case a
     when 'N'
-        pos += c * 1i
+      pos += c * 1i
     when 'S'
-        pos -= c * 1i
+      pos -= c * 1i
     when 'E'
-        pos += c
+      pos += c
     when 'W'
-        pos -= c
+      pos -= c
     when 'L'
-        dir *= (Math::E + 0i) **  (1i * c * Math::PI / 180.0)
+      dir *= (Math::E + 0i)**(1i * c * Math::PI / 180.0)
     when 'R'
-        dir *= (Math::E + 0i) ** (-1i * c * Math::PI / 180.0)
+      dir *= (Math::E + 0i)**(-1i * c * Math::PI / 180.0)
     when 'F'
-        pos += dir * c
+      pos += dir * c
     end
   end
 
@@ -34,26 +34,26 @@ end
 
 def part2
   waypos = 10 + 1i
-  pos  = 0i
+  pos = 0i
 
   $input_lines.each do |a, c|
     case a
     when 'N'
-        waypos += c * 1i
+      waypos += c * 1i
     when 'S'
-        waypos -= c * 1i
+      waypos -= c * 1i
     when 'E'
-        waypos += c
+      waypos += c
     when 'W'
-        waypos -= c
+      waypos -= c
     when 'L'
-        waypos = pos + (waypos - pos) * (Math::E + 0i) ** (1i * c * Math::PI / 180.0)
+      waypos = pos + (waypos - pos) * (Math::E + 0i)**(1i * c * Math::PI / 180.0)
     when 'R'
-        waypos = pos + (waypos - pos) * (Math::E + 0i) ** (-1i * c * Math::PI / 180.0)
+      waypos = pos + (waypos - pos) * (Math::E + 0i)**(-1i * c * Math::PI / 180.0)
     when 'F'
-        w = waypos - pos
-        pos += (waypos - pos) * c
-        waypos = pos + w
+      w = waypos - pos
+      pos += (waypos - pos) * c
+      waypos = pos + w
     end
   end
 
