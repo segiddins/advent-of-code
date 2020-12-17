94f81b9b5cde24a8bcb3ceef77479bdec0432bbd
Samuel Giddins <segiddins@squareup.com>
2020-12-16 21:33:20 -0800

    Refactor day 17

--- 2020/day_17.rb
+++ 2020/day_17.rb
@@ -3,77 +3,28 @@
 require_relative '../aoc'
 
 $input = File.read(__FILE__.sub(/\.rb\z/, '.txt'))
-$input = DATA.read
+# $input = DATA.read
 $input_lines = $input.split("\n")
 
+FourDPoint = Struct.new(:x, :y, :z, :w)
+
+$map = Hash.new('.').tap do |map|
+  $input_lines.each_with_index do |row, r|
+    row.chars.each_with_index do |a, c|
+      map[FourDPoint.new(r, c, 0, 0)] = a
+    end
+  end
+end.freeze
+
 def run(abs_dw:)
-
-end
-
-def part1
-  map = Hash.new { |h, k| h[k] = '.' }
-  $input_lines.each_with_index do |row, r|
-    row.chars.each_with_index do |a, c|
-      map[[r, c, 0]] = a
-    end
-  end
-
+  map = $map
   1.upto(6) do |_iter|
     nm = map.dup
 
-    min_x, max_x = map.map { |k, _v| k[0] }.minmax
-    min_y, max_y = map.map { |k, _v| k[1] }.minmax
-    min_z, max_z = map.map { |k, _v| k[2] }.minmax
-
-    min_x.pred.upto(max_x.succ) do |x|
-      min_y.pred.upto(max_y.succ) do |y|
-        min_z.pred.upto(max_z.succ) do |z|
-          an = 0
-
-          -1.upto(1) do |dx|
-            -1.upto(1) do |dy|
-              -1.upto(1) do |dz|
-                next if dx.zero? && dy.zero? && dz.zero?
-
-                x_ = x + dx
-                y_ = y + dy
-                z_ = z + dz
-
-                an += 1 if map[[x_, y_, z_]] == '#'
-              end
-            end
-          end
-
-          if map[[x, y, z]] == '#'
-            nm[[x, y, z]] = '.' unless (an == 2) || (an == 3)
-          else
-            nm[[x, y, z]] = '#' if an == 3
-          end
-        end
-      end
-    end
-
-    map = nm
-  end
-
-  map.count { |_, v| v == '#' }
-end
-
-def part2
-  map = Hash.new { |h, k| h[k] = '.' }
-  $input_lines.each_with_index do |row, r|
-    row.chars.each_with_index do |a, c|
-      map[[r, c, 0, 0]] = a
-    end
-  end
-
-  1.upto(6) do |_iter|
-    nm = map.dup
-
-    min_x, max_x = map.map { |k, _v| k[0] }.minmax
-    min_y, max_y = map.map { |k, _v| k[1] }.minmax
-    min_z, max_z = map.map { |k, _v| k[2] }.minmax
-    min_w, max_w = map.map { |k, _v| k[3] }.minmax
+    min_x, max_x = map.each_key.map(&:x).minmax
+    min_y, max_y = map.each_key.map(&:y).minmax
+    min_z, max_z = map.each_key.map(&:z).minmax
+    min_w, max_w = map.each_key.map(&:w).minmax
 
     min_x.pred.upto(max_x.succ) do |x|
       min_y.pred.upto(max_y.succ) do |y|
@@ -84,24 +35,21 @@ def part2
             -1.upto(1) do |dx|
               -1.upto(1) do |dy|
                 -1.upto(1) do |dz|
-                  -1.upto(1) do |dw|
+                  (-abs_dw).upto(abs_dw) do |dw|
                     next if dx.zero? && dy.zero? && dz.zero? && dw.zero?
 
-                    x_ = x + dx
-                    y_ = y + dy
-                    z_ = z + dz
-                    w_ = w + dw
-
-                    an += 1 if map[[x_, y_, z_, w_]] == '#'
+                    an += 1 if map[FourDPoint.new(x + dx, y + dy, z + dz, w + dw)] == '#'
                   end
                 end
               end
             end
 
-            if map[[x, y, z, w]] == '#'
-              nm[[x, y, z, w]] = '.' unless (an == 2) || (an == 3)
+            coord = FourDPoint.new(x, y, z, w)
+
+            if map[coord] == '#'
+              nm[coord] = '.' unless (an == 2) || (an == 3)
             else
-              nm[[x, y, z, w]] = '#' if an == 3
+              nm[coord] = '#' if an == 3
             end
           end
         end
@@ -112,6 +60,14 @@ def part2
   end
 
   map.count { |_, v| v == '#' }
+end
+
+def part1
+  run(abs_dw: 0)
+end
+
+def part2
+  run(abs_dw: 1)
 end
 
 pp part1
