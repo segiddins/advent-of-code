8c2a4bf0c5c310d581ef3ded0d4053366d9db732
Samuel Giddins <segiddins@squareup.com>
2020-12-20 21:12:32 -0800

    RuboCop

--- 2020/day_21.rb
+++ 2020/day_21.rb
@@ -5,33 +5,34 @@ require_relative '../aoc'
 $input = File.read(__FILE__.sub(/\.rb\z/, '.txt'))
 # $input = DATA.read
 $lines = $input.split("\n")
-$recipes = $lines.map { |l| ing, all, _ = l.split(/ \(contains |\)/); [ing.split(' '), all.split(', ')] }
+$recipes = $lines.map { |l| ing, all, = l.split(/ \(contains |\)/); [ing.split(' '), all.split(', ')] }
 
 def part1
-    all = Hash.new { |h, k| h[k] = [] }
-    $recipes.each do |ing, a|
-        a.each {|e| all.key?(e) ? all[e] &= ing : all[e] = ing }
-    end
-    ($recipes.map(&:first).reduce(&:+) - all.values.reduce(&:+)).size
+  all = Hash.new { |h, k| h[k] = [] }
+  $recipes.each do |ing, a|
+    a.each { |e| all.key?(e) ? all[e] &= ing : all[e] = ing }
+  end
+  ($recipes.map(&:first).reduce(&:+) - all.values.reduce(&:+)).size
 end
 
 def part2
-    all = Hash.new { |h, k| h[k] = [] }
-    $recipes.each do |ing, a|
-        a.each {|e| all.key?(e) ? all[e] &= ing : all[e] = ing }
-    end
-    canon = {}
-    loop do
-        a, i = all.find { |_, v| v.size == 1 }
-        break unless a
-        i = i[0]
+  all = Hash.new { |h, k| h[k] = [] }
+  $recipes.each do |ing, a|
+    a.each { |e| all.key?(e) ? all[e] &= ing : all[e] = ing }
+  end
+  canon = {}
+  loop do
+    a, i = all.find { |_, v| v.size == 1 }
+    break unless a
 
-        canon[a] = i
+    i = i[0]
 
-        all.delete(a)
-        all.each_value { |v| v.delete(i) }
-    end
-    canon.sort_by(&:first).map(&:last).join(',')
+    canon[a] = i
+
+    all.delete(a)
+    all.each_value { |v| v.delete(i) }
+  end
+  canon.sort_by(&:first).map(&:last).join(',')
 end
 
 pp part1
