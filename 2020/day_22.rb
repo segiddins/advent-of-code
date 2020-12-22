295d926a76fc2d820970783daae47965eaf51eb1
Samuel Giddins <segiddins@squareup.com>
2020-12-22 12:05:46 -0800

    Small speedup

--- 2020/day_22.rb
+++ 2020/day_22.rb
@@ -26,41 +26,45 @@ def part1
   c($players).each_value.sum { |d| d.reverse_each.each_with_index.sum { |c, i| c * i.succ } }
 end
 
-def rc(decks)
-  decks = decks.transform_values(&:dup)
+def rc(d1, d2)
   seen = Set.new
 
   loop do
-    return { '1' => decks['1'] } unless seen.add?(decks.transform_values(&:dup))
-    break if decks.each_value.any?(&:empty?)
+    break if d1.empty? || d2.empty?
 
-    c1, c2 = *decks.each_value.map(&:shift)
+    k = [d1.dup, d2.dup]
+    break d2 = [] unless seen.add?(k)
 
-    if decks['1'].size >= c1 && decks['2'].size >= c2
-      r = rc({ '1' => decks['1'][0, c1], '2' => decks['2'][0, c2] })
-      if r['1']
-        decks['1'].concat [c1, c2]
+    c1 = d1.shift
+    c2 = d2.shift
+
+    if d1.size >= c1 && d2.size >= c2
+      r = rc(d1[0, c1], d2[0, c2])
+      if r[0]
+        d1.concat [c1, c2]
       else
-        decks['2'].concat [c2, c1]
+        d2.concat [c2, c1]
       end
     else
       if c1 > c2
-        decks['1'].concat [c1, c2]
+        d1.concat [c1, c2]
       else
-        decks['2'].concat [c2, c1]
+        d2.concat [c2, c1]
       end
     end
   end
 
-  decks.reject { |_k, v| v.empty? }
+  [d1, d2].map { |v| v.empty? ? nil : v }
 end
 
 def part2
-  rc($players).each_value.sum { |d| d.reverse_each.each_with_index.sum { |c, i| c * i.succ } }
+  rc(*$players.sort_by(&:first).map(&:last)).compact.sum { |d| d.reverse_each.each_with_index.sum { |c, i| c * i.succ } }
 end
 
 pp part1
 pp part2
+
+# pp $states.group_by(&:itself).transform_values(&:count).sort_by(&:last)
 
 __END__
 Player 1:
