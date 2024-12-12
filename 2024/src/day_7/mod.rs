c89dcb46a451138b9ebdc7286be64ea802134d80
Samuel Giddins <segiddins@segiddins.me>
2024-12-12 10:53:12 -0800

    Enquicken day 7
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- 2024/src/day_7/mod.rs
+++ 2024/src/day_7/mod.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
 use std::fmt::Debug;
+use std::{collections::HashMap, error::Error};
 
 use itertools::{repeat_n, Itertools};
-use rayon::iter::{ParallelBridge, ParallelIterator};
+use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
 
 #[derive(Debug)]
 pub struct Solution {
@@ -34,49 +34,69 @@ fn concat(a: i64, b: i64) -> Option<i64> {
 
 impl crate::Solution for Solution {
     fn part_1(&self) -> Result<i64, Box<dyn Error>> {
-        let mut equations = self.equations.clone();
-        equations.retain(|(lhs, rhs)| {
-            repeat_n(vec![i64::checked_add, i64::checked_mul], rhs.len() - 1)
-                .into_iter()
-                .multi_cartesian_product()
-                .par_bridge()
-                .any(|mut ops| {
-                    let res = rhs
-                        .iter()
-                        .map(|v| *v)
-                        .reduce(|a, b| {
-                            let f = ops.pop().unwrap();
-                            f(a, b).unwrap()
-                        })
-                        .unwrap();
-                    // println!("{:?} = {:?} -> {:?} => {}", lhs, rhs, ops, res);
-                    *lhs == res
-                })
-        });
-        Ok(equations.iter().map(|(lhs, _)| lhs).sum::<i64>())
+        Ok(self
+            .equations
+            .par_iter()
+            .filter(|(lhs, rhs)| {
+                repeat_n(vec![i64::checked_add, i64::checked_mul], rhs.len() - 1)
+                    .into_iter()
+                    .multi_cartesian_product()
+                    .par_bridge()
+                    .any(|mut ops| {
+                        let res = rhs
+                            .iter()
+                            .map(|v| *v)
+                            .reduce(|a, b| {
+                                let f = ops.pop().unwrap();
+                                f(a, b).unwrap()
+                            })
+                            .unwrap();
+                        // println!("{:?} = {:?} -> {:?} => {}", lhs, rhs, ops, res);
+                        *lhs == res
+                    })
+            })
+            .map(|(lhs, _)| lhs)
+            .sum::<i64>())
     }
 
     fn part_2(&self) -> Result<i64, Box<dyn Error>> {
-        let mut equations = self.equations.clone();
         let op_options = vec![i64::checked_add, i64::checked_mul, concat];
-        let mut counts = equations.iter().counts_by(|(_, rhs)| rhs.len());
-        println!("{:?}", counts);
-        equations.retain(|(lhs, rhs)| {
-            repeat_n(&op_options, rhs.len() - 1)
-                .multi_cartesian_product()
-                .par_bridge()
-                .any(|mut ops| {
+        let counts = self
+            .equations
+            .iter()
+            .map(|(_, rhs)| rhs.len())
+            .unique()
+            .fold(
+                HashMap::new(),
+                |mut acc: HashMap<usize, Vec<Vec<&fn(i64, i64) -> Option<i64>>>>, len| {
+                    acc.entry(len).or_insert(
+                        repeat_n(&op_options, len - 1)
+                            .multi_cartesian_product()
+                            .collect_vec(),
+                    );
+                    acc
+                },
+            );
+        let answers = self
+            .equations
+            .par_iter()
+            .filter(|(lhs, rhs)| {
+                counts.get(&rhs.len()).unwrap().iter().any(|ops| {
                     let res = rhs
                         .iter()
                         .map(|v| *v)
-                        .reduce(|a, b| {
-                            let f = ops.pop().unwrap();
-                            f(a, b).unwrap()
+                        .enumerate()
+                        .reduce(|(i, a), (j, b)| {
+                            let f = ops[i];
+                            (j, f(a, b).unwrap())
                         })
-                        .unwrap();
+                        .unwrap()
+                        .1;
                     res == *lhs
                 })
-        });
-        Ok(equations.iter().map(|(lhs, _)| *lhs).sum::<i64>())
+            })
+            .map(|(lhs, _)| *lhs)
+            .sum::<i64>();
+        Ok(answers)
     }
 }
